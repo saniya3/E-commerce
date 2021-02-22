7e9028d45ef4ac650e48794cff220318ced99bf3
Saniya Ahuja <ahuja.saniya@gmail.com>
2021-02-22 10:45:24 +0530

    updated

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+
+const app=express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.get("/clothing",function(req,res){
+    res.sendFile(__dirname+"/clothing.html");
+});
+
+app.get("/mobiles",function(req,res){
+    res.sendFile(__dirname+"/mobiles.html");
+});
+
+app.get("/electronics",function(req,res){
+    res.sendFile(__dirname+"/electronics.html");
+});
+
+app.get("/bestSellers",function(req,res){
+    res.sendFile(__dirname+"/best.html");
+});
+
+app.get("/biba-salwar-suit-set", function(req,res){
+    res.sendFile(__dirname+"/biba-salwar-suit-set.html");
+});
+
+app.post("/",function(req,res){
   
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
+});
+
+app.listen(3000, function() {
+    console.log("Server started");
+});
